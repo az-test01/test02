4a66b5b9ffe00bc40f0e7cb4a62bdac98c704c29
az-test02 <az-test02@localhost.localdomain>
2013-02-07 16:15:30 +0900

    test

---
+++ rb01/test1-1.rb
@@ -0,0 +1,9 @@
+print("Hello World!!") ## コメント 
+
+=begin
+複数行
+コメントは
+こんな感じ
+=end
+
+
---
+++ rb01/test1-2.rb
@@ -0,0 +1,7 @@
+print("Hello World!!")
+puts "Hello World, again" # 改行付きで表示
+p "Hello World 3" # オブジェクト型が分かるように表示 
+ 
+
+
+
